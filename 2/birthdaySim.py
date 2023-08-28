97e51e32f74a6d72ade6d9a14a991cacafef758e
Brian Tierney <bwt8686@gmail.com>
2023-08-28 18:50:54 -0400

    start second challenge

---
+++ 2/birthdaySim.py
@@ -0,0 +1,24 @@
+#Challenge is Birthday Paradox Simulation, by Al Sweigart al@inventwithpython.com
+
+#import datetime and random libraries
+import datetime, random
+
+#todo: create functions to generate set of random birthdays given a number of birthdays to create 
+def generateBirthdatys(numberToGenerate):
+    return 1
+
+#todo: create funtion to run 100000 simulations of birhtday generation
+def runSimulations():
+    return 1
+
+#todo: create funtions to get statistics of simulation
+def getSimStats(simData):
+    return 1
+
+#main function
+def main():
+    print("hello")
+
+# If the program is run, run the simuilation:
+if __name__ == '__main__':
+    main()
