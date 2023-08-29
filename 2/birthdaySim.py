f5fc957809097a350f9caca957a550a3f5c09ed3
Brian Tierney <bwt8686@gmail.com>
2023-08-29 18:30:52 -0400

    coded simulation and analysis (will probably come back and recomment code before I call it complete)

--- 2/birthdaySim.py
+++ 2/birthdaySim.py
@@ -4,20 +4,42 @@
 import datetime, random
 
 #todo: create functions to generate set of random birthdays given a number of birthdays to create 
-def generateBirthdatys(numberToGenerate):
-    return 1
+def generateBirthdays(numberToGenerate):
+    randomBirthdays = []
+    for i in range(numberToGenerate):
+        randDays   = random.randint(0, 364)
+        randomBirthdays.append(datetime.date(2023,1,1)+ datetime.timedelta(days=randDays))
+    return randomBirthdays 
 
 #todo: create funtion to run 100000 simulations of birhtday generation
-def runSimulations():
-    return 1
+def runSimulations(simsToRun = 1, bDaysPerSim = 25):
+    print(f'Generating a set of {bDaysPerSim} birthdays {simsToRun} times...')
+    sims = []
+    for i in range(simsToRun):
+        sims.append(generateBirthdays(bDaysPerSim))
+        if round(i/simsToRun, 6) == .1:
+            print("10% Generated...")
+        if round(i/simsToRun, 6) == .5:
+            print("50% Generated...")
+        if round(i/simsToRun, 6) == .9:
+            print("90% Generated...")
+        if round(i/simsToRun, 6) == 1:
+            print("100% Generated...")
+    return sims
 
 #todo: create funtions to get statistics of simulation
 def getSimStats(simData):
-    return 1
+    print("Analyzing Simulations...")
+    matchCount = 0
+    for sim in simData:
+        if len(set(sim)) < len(sim):
+            matchCount += 1
+    print(f'Out of {len(simData)} sims run, there was a matching birthday {matchCount} times.\nThat means the percentage of the time matching birthdays occured in simulations of {len(sim)} people was {round((matchCount/len(simData)) * 100, 4)}%!') 
 
 #main function
 def main():
-    print("hello")
+    numBDays = int(input('How many Birthdays should be generated per simulation?\n'))
+    getSimStats(runSimulations(100000,numBDays))
 
 # If the program is run, run the simuilation:
 if __name__ == '__main__':
